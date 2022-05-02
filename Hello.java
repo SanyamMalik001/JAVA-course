e7972e2a11c8b49e59221461ff25d1cd540cb67a
Sanyam Malik <50053596+SanyamMalik001@users.noreply.github.com>
2022-05-02 12:29:54 +0530

    This is a very basic challenge of variable.

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+// Variable challenge
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello World");
+        int myFirstNumber = (10+5)+(2*10);
+        int MySecondNumber=12;
+        int myThirdNumber = myFirstNumber*2;
+        int myTotal = myFirstNumber+ MySecondNumber+ myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne  = 1000-myTotal;
+        System.out.println(myLastOne);
+
+
+    }
+}
