9773d6290b503cfad0659dcebe423e867a8404f1
Sanyam Malik <50053596+SanyamMalik001@users.noreply.github.com>
2022-05-24 08:15:21 +0530

    Converting KB into MB and KB

---
+++ MegaBytesConverter.java
@@ -0,0 +1,16 @@
+package com.learningjavapackage;
+
+
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        int MB, KB;
+        if(kiloBytes<0)
+            System.out.println("Invalid Value");
+        else {
+            MB = kiloBytes / 1024;
+            KB = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + MB + " MB and " + KB + " KB");
+        }
+    }
+
+}
