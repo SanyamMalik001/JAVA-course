8f7c26d1aa4d8bce0cc36b71bf433eb0ab48aff1
Sanyam Malik <50053596+SanyamMalik001@users.noreply.github.com>
2022-05-24 12:14:00 +0530

    Find if a year is leap or not

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+package com.learningjavapackage;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if( year<1 || year>9999)
+            return false;
+        else{
+
+            if( (year %400==0)||(year%100 !=0 && year%4==0))
+                return true;
+            else return false;
+        }
+
+    }
+}
