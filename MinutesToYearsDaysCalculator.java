c2b247dbe002b44e33bbc07e4766c16f60683c5d
Sanyam Malik <50053596+SanyamMalik001@users.noreply.github.com>
2022-05-28 09:15:46 +0530

    CE9: Minutes to Years and Days

---
+++ MinutesToYearsDaysCalculator.java
@@ -0,0 +1,11 @@
+package com.learningjavapackage;
+
+public class MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        long years,days;
+        if (minutes<0)
+            System.out.println("Invalid Value");
+        else
+            System.out.println(minutes+" min = "+minutes/(60*24*365)+" y and "+ (minutes/(60*24))%365 +"d");
+    }
+}
